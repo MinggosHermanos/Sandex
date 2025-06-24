59c875a52263e8e4993c8629fd04edb7e36fdf87
MinggosHermanos <sakum5555@gmail.com>
2025-06-25 02:17:40 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Konfigurasi ini tidak memerlukan file postcss.config.js lagi
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
