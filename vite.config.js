18dbd2f2c18e65b3bd815131ca284d4f285e9e39
MinggosHermanos <sakum5555@gmail.com>
2025-06-25 19:40:07 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
-// https://vitejs.dev/config/
+// Konfigurasi final yang menyatukan semuanya
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
 })
