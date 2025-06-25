35961adbbd737d5e05880e3d439f0b6c6d0dd43d
MinggosHermanos <sakum5555@gmail.com>
2025-06-25 19:45:36 +0700

    Update tailwind.cofig.js

--- tailwind.cofig.js
+++ tailwind.cofig.js
@@ -1,31 +1,12 @@
-// 1. Ganti seluruh isi tailwind.config.js Anda dengan ini:
-
-/\*\* @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-content: [
-"./index.html",
-"./src/\*\*/*.{js,ts,jsx,tsx}",
-],
-theme: {
-extend: {},
-},
-plugins: [],
-darkMode: 'class',
-}
-
-```javascript
-// 2. Ganti seluruh isi vite.config.js Anda dengan ini:
-// (Ini adalah perubahan paling penting)
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-
-export default defineConfig({
-  plugins: [react()],
-  css: {
-    postcss: {
-      plugins: [tailwindcss()],
-    },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
   },
-})
+  plugins: [],
+  darkMode: 'class',
+}
